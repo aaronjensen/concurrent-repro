5bbddd5d49d17214c6c3ba5ba5aa5eb7354657f4
Aaron Jensen <aaronjensen@gmail.com>
2022-01-10 12:21:09 -0500

    Clarification

--- repro.rb
+++ repro.rb
@@ -3,9 +3,7 @@ require "bundler/setup"
 require "concurrent-ruby"
 
 def sub
-  yield "some-value"
-
-  nil
+  yield
 end
 
 def run
@@ -17,8 +15,12 @@ def run
 
   promises = [
     Concurrent::Promise.execute(executor: executor) do
+      # This version fails on Ruby 3.0.3 as well
+      # yield "some-value"
+
+      # This version only fails in Ruby 3.1.0
       sub do |value|
-        yield value
+        yield "some-value"
       end
     end
   ]
