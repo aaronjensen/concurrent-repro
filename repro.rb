1682c83a601be2f9b40cbd844d296bcdde433efa
Aaron Jensen <aaronjensen@gmail.com>
2022-01-10 12:18:34 -0500

    Repro is updated so that it passes in Ruby 3.0.3

--- repro.rb
+++ repro.rb
@@ -2,6 +2,12 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("./Gemfile", __dir__)
 require "bundler/setup"
 require "concurrent-ruby"
 
+def sub
+  yield "some-value"
+
+  nil
+end
+
 def run
   return to_enum(__method__) unless block_given?
 
@@ -11,7 +17,9 @@ def run
 
   promises = [
     Concurrent::Promise.execute(executor: executor) do
-      yield "some-value"
+      sub do |value|
+        yield value
+      end
     end
   ]
   promises.each(&:wait!)
