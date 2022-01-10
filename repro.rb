458299129c36ec6818508ff2c5b8a2f8dbe56e16
Aaron Jensen <aaronjensen@gmail.com>
2022-01-10 11:34:18 -0500

    Narrow repro

--- repro.rb
+++ repro.rb
@@ -1,10 +1,11 @@
-require_relative "./config/boot"
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("./Gemfile", __dir__)
+require "bundler/setup"
 require "sprockets"
 
 environment = Sprockets::Environment.new(Dir.pwd)
-environment.append_path("./app/assets/stylesheets")
+environment.append_path(".")
 
-manifest = Sprockets::Manifest.new(environment, "./app/assets/config/manifest.js")
+manifest = Sprockets::Manifest.new(environment, "./manifest.js")
 manifest.find_sources("application.css").first
 
 puts "It worked"
